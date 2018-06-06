436e989f57ee9133d91a20687b6414fad640a4f5
Jingyu Yao <yjy.emails@gmail.com>
2018-06-06 01:16:33 -0400

    added ping service to test api calling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PingService } from './ping.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+
+  constructor(private pingService: PingService) { }
+
+  ngOnInit() {
+    this.pingService.getPong().subscribe(pong => console.log(pong));
+  }
 }
---
+++ src/app/ping.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { ConfigService } from './config.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PingService {
+
+  constructor(
+    private http: HttpClient,
+    private configService: ConfigService) { }
+
+  getPong(): Observable<Pong> {
+    return this.http.get<Pong>(this.configService.getApiUrl('/ping'));
+  }
+}
+
+export interface Pong {
+  data: string;
+}
