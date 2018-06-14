26cc402ffdab39febf217bbf2aa162634acea2d4
Jingyu Yao <yjy.emails@gmail.com>
2018-06-14 19:23:15 -0400

    material form

--- src/app/app-material.module.ts
+++ src/app/app-material.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
 import { MatTableModule } from '@angular/material/table';
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -7,12 +9,16 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 @NgModule({
   imports: [
     MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
     MatListModule,
     MatTableModule,
     MatToolbarModule,
   ],
   exports: [
     MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
     MatListModule,
     MatTableModule,
     MatToolbarModule,
