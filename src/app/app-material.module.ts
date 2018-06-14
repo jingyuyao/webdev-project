f3029e6aba1a6eae5420ea75295b9f994a27554a
Jingyu Yao <yjy.emails@gmail.com>
2018-06-14 13:13:10 -0400

    simple deck-list

--- src/app/app-material.module.ts
+++ src/app/app-material.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   imports: [
     MatButtonModule,
+    MatListModule,
     MatToolbarModule,
   ],
   exports: [
     MatButtonModule,
+    MatListModule,
     MatToolbarModule,
   ],
 })
--- src/app/deck-list/deck-list.component.ts
+++ src/app/deck-list/deck-list.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+
+import { Deck } from '../models/deck.model';
 
 @Component({
   selector: 'app-deck-list',
   templateUrl: './deck-list.component.html',
   styleUrls: ['./deck-list.component.css']
 })
-export class DeckListComponent implements OnInit {
+export class DeckListComponent {
+  @Input() decks: Deck[];
+  @Output() selected = new EventEmitter<Deck>();
 
   constructor() { }
-
-  ngOnInit() {
-  }
-
 }
