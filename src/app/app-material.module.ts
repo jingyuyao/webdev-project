ec280bfe7591b6b1dd6f2f00d530aca70c781ec2
Jingyu Yao <yjy.emails@gmail.com>
2018-06-14 16:43:33 -0400

    simple cards table display

--- src/app/app-material.module.ts
+++ src/app/app-material.module.ts
@@ -1,17 +1,20 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatListModule } from '@angular/material/list';
+import { MatTableModule } from '@angular/material/table';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   imports: [
     MatButtonModule,
     MatListModule,
+    MatTableModule,
     MatToolbarModule,
   ],
   exports: [
     MatButtonModule,
     MatListModule,
+    MatTableModule,
     MatToolbarModule,
   ],
 })
--- src/app/hs-card-table/hs-card-table.component.ts
+++ src/app/hs-card-table/hs-card-table.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+
+import { Card } from '../models/card.model';
 
 @Component({
   selector: 'app-hs-card-table',
@@ -6,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./hs-card-table.component.css']
 })
 export class HsCardTableComponent implements OnInit {
+  @Input() cards: Card[];
+  displayedColumns = ['id'];
 
   constructor() { }
 
