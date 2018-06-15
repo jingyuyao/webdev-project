e3e47e70153b00da247f2ad89bbc2054091124c1
Jingyu Yao <yjy.emails@gmail.com>
2018-06-14 23:20:30 -0400

    take > first

--- src/app/resolvers/user.resolver.ts
+++ src/app/resolvers/user.resolver.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Router, Resolve, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { Observable } from 'rxjs';
-import { first, tap } from 'rxjs/operators';
+import { take, tap } from 'rxjs/operators';
 
 import { User } from '../models/user.model';
 import { UserService } from '../services/user.service';
@@ -20,7 +20,7 @@ export class UserResolver implements Resolve<User> {
     state: RouterStateSnapshot,
   ): Observable<User> {
     return this.userService.currentUser().pipe(
-      first(),
+      take(1),
       tap(user => {
         if (!user) {
           alert(`You need to be logged in to visit ${state.url}`);
